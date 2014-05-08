642529b4b702f0998b8702f3db149e1599255ebb
Davide Ciminaghi <ciminaghi@gnudd.com>
2014-05-08 16:28:49 +0200

    MCUIO: add host controller device related functions
    Signed-off-by: Davide Ciminaghi <ciminaghi@gnudd.com>

---
+++ drivers/mcuio/mcuio-hc-dev.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2011 Dog Hunter SA
+ * Author: Davide Ciminaghi <ciminaghi@gnudd.com>
+ *
+ * GNU GPLv2 or later
+ */
+
+/* mcuio host controller functions */
+
+#include <linux/mcuio.h>
+#include <linux/circ_buf.h>
+#include <linux/slab.h>
+#include <linux/sched.h>
+#include <linux/regmap.h>
+#include <linux/err.h>
+#include <linux/workqueue.h>
+#include <linux/circ_buf.h>
+#include <linux/mcuio_ids.h>
+#include <linux/mcuio-hc.h>
+#include "mcuio-internal.h"
+
+static struct mcuio_device_id default_hc_id = {
+	.device = 0,
+	.vendor = 0,
+	.class = MCUIO_CLASS_HOST_CONTROLLER,
+};
+
+struct device *mcuio_add_hc_device(struct mcuio_device_id *id, rfun rf,
+				   void *data)
+{
+	int b, ret = -ENOMEM;
+	struct mcuio_device *d = kzalloc(sizeof(*d), GFP_KERNEL);
+	if (!d)
+		return ERR_PTR(-ENOMEM);
+	b = mcuio_get_bus();
+	if (b < 0) {
+		ret = b;
+		goto err0;
+	}
+	d->bus = b;
+	d->device = 0;
+	d->fn = 0;
+	d->id = id ? *id : default_hc_id;
+	d->do_request = rf;
+	d->do_request_data = data;
+	ret = mcuio_device_register(d, NULL, NULL);
+	if (ret < 0)
+		goto err1;
+	return &d->dev;
+
+err1:
+	mcuio_put_bus(b);
+err0:
+	kfree(d);
+	return ERR_PTR(ret);
+}
+EXPORT_SYMBOL(mcuio_add_hc_device);
+
+MODULE_AUTHOR("Davide Ciminaghi");
+MODULE_DESCRIPTION("MCUIO host controller code");
+MODULE_LICENSE("GPL v2");
--- include/linux/mcuio-hc.h
+++ include/linux/mcuio-hc.h
@@ -9,6 +9,8 @@
 #ifndef __HOST_CONTROLLER_H__
 #define __HOST_CONTROLLER_H__
 
+#include <linux/mcuio.h>
+
 #define MCUIO_HC_OUTBUF 0x8
 #define MCUIO_HC_INBUF 0x108
 /* How many 32bits words are in rx buffer */
@@ -22,5 +24,15 @@
 #define MCUIO_IRQ_CLR  0x214
 
 #define MCUIO_HC_MAX_REGISTER 0x214
+
+/*
+ * mcuio_add_hc_device() : add host controller device
+ *
+ * @id: pointer to mcuio device's id
+ * @rf: pointer to request function.
+ * @data: data passed to rf callback.
+ */
+struct device *mcuio_add_hc_device(struct mcuio_device_id *id, rfun rf,
+				   void *data);
 
 #endif /* __HOST_CONTROLLER_H__ */
